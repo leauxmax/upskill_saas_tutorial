acd5b5b53c61e179b56e7d938bc9e3ffb7e3e580
Loren Schmidt <lorenmschmidt@gmail.com>
2016-12-18 06:15:08 +0000

    corrected email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'lorenmschmidt@gmail.com'
+  default to: 'kaizensozay@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
