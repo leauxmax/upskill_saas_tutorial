3381b8997b684277d9cfeba25bddf5782c587c18
Loren Schmidt <lorenmschmidt@gmail.com>
2017-04-19 13:54:50 +0000

    NOW_contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'kaizensozay@gmail.com'
+  default to: 'lorenmschmidt@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
